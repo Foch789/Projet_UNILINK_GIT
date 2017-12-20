cc6b025035071188645b5e46729f6e719e06bc14
alexandre <alexandre@localhost.localdomain>
2017-12-20 22:52:02 +0100

    Modification de recherche.inc.php

---
+++ application/modules/recherche.inc.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'configBDD.inc.php';
+
+
+
+try
+{
+  $requete = $PDO_BDD -> prepare("Select nom_etu, prenom_etu, intitule_comp from etudiant natural join competence where intitule_comp=:comp");
+  $requete->execute(array('comp'=>$form));
+}
+
+catch(Exception $e)
+{
+  die(echo 'Erreur'.$e->getMessage().'</br>');
+}
+
+
+while($line = $requete->fetch(PDO::FETCH_OBJ))
+  echo $line->nom_etu.' '.$line->prenom_etu.' '.$line->intitule_comp.'</br>';
+?>
