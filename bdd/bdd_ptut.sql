93845030f8d54cd6d92391d7a9203c82d02e2091
alexandre <alexandre@localhost.localdomain>
2018-02-19 14:48:06 +0100

    plop

--- bdd/bdd_ptut.sql
+++ bdd/bdd_ptut.sql
@@ -1,35 +1,35 @@
 Create table promo
 (
-	id_promo int primary key,
+	id_promo serial primary key,
 	intitule_promo varchar(10)
 );
-
+	
 Create table etudiant
 (
-	id_etudiant int primary key,
+	id_etu serial primary key,
 	nom_etu varchar(20),
 	prenom_etu varchar(20),
-	aidant int,
-	email varchar(50),
-	mdp varchar(20),
-	description text,
+	aidant_etu int,
+	email_etu varchar(50),
+	mdp_etu varchar(20),
+	desc_etu text,
 	id_promo int,
 	constraint FK_ETU_PROMO foreign key (id_promo) references promo (id_promo),
-	constraint FK_AIDE foreign key (aidant) references etudiant(id_etudiant)
+	constraint FK_AIDE foreign key (aidant_etu) references etudiant(id_etu)
 );
 
 Create table competence
 (
-	id_comp int primary key,
+	id_comp serial primary key,
 	intitule_comp varchar(100)
 );
 
 Create table niveau
 (
 	niveau int,
-	id_etudiant int,
-	id_comp int,
-	constraint FK_ETU_NIV foreign key (id_etudiant) references etudiant(id_etudiant),
+	id_etu serial,
+	id_comp serial,
+	constraint FK_ETU_NIV foreign key (id_etu) references etudiant(id_etu),
 	constraint FK_COMP_NIV foreign key (id_comp) references competence(id_comp)
 );
 
@@ -38,9 +38,11 @@ Create table aide
 (
 	aidant boolean,
 	demande_aide boolean,
-	accepter boolean,
-	id_etudiant int,
-	id_comp int,
-	constraint FK_ETU_AIDE foreign  key (id_etudiant) references etudiant(id_etudiant),
+	accepter_aide boolean,
+	id_aide serial,
+	id_aidant serial,
+	id_comp serial,
+	constraint FK_ETU_AIDE foreign  key (id_aide) references etudiant(id_etu),
+	constraint FK_ETU_AIDANT foreign  key (id_aidant) references etudiant(id_etu),
 	constraint FK_COMP_AIDE foreign key (id_comp) references competence(id_comp)
 );
